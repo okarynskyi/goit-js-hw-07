e9fe84dc7d62a0cac7387839f99ba142618d362e
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-20 22:27:15 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = createLi(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+gallery.addEventListener('click', listHandler);
+
+function createLi(array) {
+    return array.reduce((acc, item) => acc + `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>` ," ")
+}
+
+function listHandler(evt) {
+    evt.preventDefault()
+    
+    const originalImg = evt.target.dataset.source;       
+    const instance = basicLightbox.create(`<img src="${originalImg}" width="800" height="600">`,
+        {
+            onShow: () => window.addEventListener('keydown', escClick),
+            onClose: () => window.removeEventListener('keydown', escClick)
+        }
+    )
+    instance.show()
+    
+    
+    function escClick(evt) {
+        if (evt.code === 'Escape') {
+            instance.close()
+        }
+    }
+}
