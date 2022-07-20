294d7d7a14745b3c77cf456eab3af745e99f268e
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-21 00:17:19 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = createLi(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+// gallery.addEventListener('click', listHandler);
+
+function createLi(array) {
+    return array.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>` ," ")
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250'
+});
